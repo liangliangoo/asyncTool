ec2f610deacd7a3eb6fc6e3e697ad98140e32439
六月 <liuyue@yizhoucp.cn>
2023-05-08 10:54:23 +0800

    feat: 并发调度封装

--- src/main/java/com/xiaoxiong/wrapper/CompletableFutureUtils.java
+++ src/main/java/com/xiaoxiong/wrapper/CompletableFutureUtils.java
@@ -1,5 +1,6 @@
 package com.xiaoxiong.wrapper;
 
+import com.google.common.collect.Lists;
 import com.jd.platform.async.executor.Async;
 import com.jd.platform.async.wrapper.WorkerWrapper;
 
@@ -11,35 +12,49 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @Author 六月
  * @Date 2023/4/15 19:27
  * @Version 1.0
  */
-public class CompletableFutureUtils<P, R> {
+public class CompletableFutureUtils {
 
     private static final String prefix = "completableFutureName-";
-    private static final Long TIMEOUT = 1000l;
+    private static final Long TIMEOUT = 2000l;
 
     private static final ExecutorService executorService =  Executors.newCachedThreadPool();
 
+    public static <R> List<R> doParallelTask(List<Supplier<R>> functions) throws ExecutionException, InterruptedException {
+        if (functions == null || functions.isEmpty()) {
+            return Lists.newArrayList();
+        }
+        List<WorkerWrapper> workerWrappers = new ArrayList<>();
+        for (int i = 0; i < functions.size(); i++) {
+            workerWrappers.add(buildWorkWrapper(functions.get(i), String.valueOf(i)));
+        }
+        return getRunResult(workerWrappers);
+    }
 
-    public static <P, R> List<R> doParallelTask(Function<R, P>... functions) throws ExecutionException, InterruptedException {
-        if (functions.length == 0) {
+    public static <P, R> List<R> doParallelTask(List<Function<P, R>> functions, List<P> params) throws ExecutionException, InterruptedException {
+        if (functions == null || functions.isEmpty()) {
             return new ArrayList<>();
         }
-        Executors.newCachedThreadPool();
         List<WorkerWrapper> workerWrappers = new ArrayList<>();
-        for (int i = 0; i < functions.length; i++) {
-            workerWrappers.add(buildWorkWrapper(functions[i], String.valueOf(i)));
+        for (int i = 0; i < functions.size(); i++) {
+            workerWrappers.add(buildWorkWrapper(functions.get(i), String.valueOf(i), params.get(i)));
         }
+        return getRunResult(workerWrappers);
+    }
+
+    private static <R> List<R> getRunResult(List<WorkerWrapper> workerWrappers) throws ExecutionException, InterruptedException {
         Async.beginWork(TIMEOUT, executorService, workerWrappers);
         return aggregationResults(workerWrappers);
     }
 
     private static <P> List<P> aggregationResults(List<WorkerWrapper> workerWrappers) {
-        if (Objects.isNull(workerWrappers) || workerWrappers.size() == 0) {
+        if (Objects.isNull(workerWrappers) || workerWrappers.isEmpty()) {
             return new ArrayList<>();
         }
         List<P> result = new ArrayList<>();
@@ -54,10 +69,29 @@ public class CompletableFutureUtils<P, R> {
         return new WorkerWrapper.Builder<P, R>()
                 .id(workName)
                 .worker((params, allWrappers) -> {
-                    System.out.println("test");
-                   return function.apply(params);
+                    System.out.println(params);
+                    return function.apply(params);
                 })
                 .param(null)
+                .build();
+    }
+
+    private static <P, R> WorkerWrapper buildWorkWrapper(Supplier<R> function, String workName) {
+        return new WorkerWrapper.Builder<P, R>()
+                .id(workName)
+                .worker((params, allWrappers) -> function.get())
+                .param(null)
+                .build();
+    }
+
+    private static <P, R> WorkerWrapper buildWorkWrapper(Function<P, R> function, String workName, P param) {
+        return new WorkerWrapper.Builder<P, R>()
+                .id(workName)
+                .worker((params, allWrappers) -> {
+                    System.out.println(params);
+                    return function.apply(params);
+                })
+                .param(param)
                 .build();
     }
 
--- src/test/java/com/xiaoxiong/wrapper/CompletableFutureUtilsTest.java
+++ src/test/java/com/xiaoxiong/wrapper/CompletableFutureUtilsTest.java
@@ -1,5 +1,7 @@
 package com.xiaoxiong.wrapper;
 
+import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,10 +12,19 @@ class CompletableFutureUtilsTest {
 
     @Test
     void doParallelTask_test() throws ExecutionException, InterruptedException {
-        List<String> strs = CompletableFutureUtils.doParallelTask(s -> "test");
+        List<String> strs = CompletableFutureUtils.doParallelTask(Lists.newArrayList(s -> "test", s1 -> "test1", s2 -> "test2"), Lists.newArrayList("test", "test1", "test3"));
         System.out.println(strs.size());
         System.out.println(strs.get(0));
     }
+
+
+    @Test
+    void doParallelTask1_test() throws ExecutionException, InterruptedException {
+        List<String> strs = CompletableFutureUtils.doParallelTask(Lists.newArrayList(() -> "hello supplier1", () -> "hello supplier2"));
+        System.out.println(strs.size());
+        System.out.println(JSON.toJSONString(strs));
+    }
+
 
     @Test
     void function_test() {
@@ -25,12 +36,9 @@ class CompletableFutureUtilsTest {
     }
 
     private static <T, R> Function<T, R> buildFunction() {
-        return new Function<T, R>() {
-            @Override
-            public R apply(T t) {
-                System.out.println("apply");
-                return null;
-            }
+        return t -> {
+            System.out.println("apply " + Thread.currentThread().getName());
+            return (R) (t + "\t" + Thread.currentThread().getName());
         };
     }
 }
